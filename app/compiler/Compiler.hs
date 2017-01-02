12a2dba5357d628d8faf45436933a80c45b98c08
Isaac Elliott <isaace71295@gmail.com>
2017-01-02 17:56:52 +1000

    Added command line options to compiler

--- app/compiler/Compiler.hs
+++ app/compiler/Compiler.hs
@@ -1,23 +1,47 @@
 import           Control.Monad
 import           Data.Bifunctor
+import           Options.Applicative
 import           System.Environment
 
 import           Lambda.Core.Codegen
 import           Lambda.Lexer        (tokenize)
-import           Lambda.Parser       (ParseError (..), parseProgram)
+import           Lambda.Parser       (parseProgram)
+import qualified Lambda.Parser       as P (ParseError)
 import           Lambda.PHP
 import           Lambda.Sugar
 
 data CompilerError
-  = CompilerParseError ParseError
+  = CompilerParseError P.ParseError
   | CompilerLexError String
   deriving Show
 
-main :: IO ()
-main = do
-  file:_ <- getArgs
-  content <- readFile file
+data CompileOpts
+  = CompileOpts
+  { filepath  :: FilePath
+  , useStdout :: Bool
+  }
+
+parseCompileOpts :: Parser CompileOpts
+parseCompileOpts = CompileOpts <$>
+  strArgument
+    (metavar "SOURCE" <> help "Source file") <*>
+  switch
+    (long "stdout" <> help "Print source to stdout")
+
+compile :: CompileOpts -> IO ()
+compile opts = do
+  content <- readFile $ filepath opts
   let genAst = first CompilerLexError . tokenize
         >=> first CompilerParseError . parseProgram
   let ast = genAst content
-  either print (print . toSource "    " . genPHP . fmap desugar) ast
+  case toSource "    " . genPHP . fmap desugar <$> ast of
+    Left err -> print err
+    Right res
+      | useStdout opts -> print res
+      | otherwise -> writeFile (filepath opts <> ".php") res
+
+main :: IO ()
+main = execParser opts >>= compile
+  where
+    opts = info (helper <*> parseCompileOpts)
+      (fullDesc <> progDesc "Compile a Lambda source file" <> header "lambdac - Compiler for the Lambda language")
