a93953ff7afd5996dc1032ac60029b765973d39f
Isaac Elliott <isaace71295@gmail.com>
2017-01-03 15:42:02 +1000

    Implemented codegen for pattern matching on literals

--- src/Lambda/Core/Codegen.hs
+++ src/Lambda/Core/Codegen.hs
@@ -2,8 +2,10 @@
 
 module Lambda.Core.Codegen (genPHP) where
 
+import Control.Monad.Reader
 import           Control.Monad.State
 import Data.Foldable (traverse_)
+import qualified Data.List.NonEmpty as N
 import Data.Maybe
 import           Data.DList
 
@@ -20,41 +22,55 @@ toFunction (Abs varName expr) = Just $ (varName :) <$> go expr
     go expr' = (expr',[])
 toFunction _ = Nothing
 
-genFunctionBody :: Expr -> [PHPStatement]
-genFunctionBody expr
-  = let (ret,statements) = runState (genPHPExpr expr) empty
-    in toList $ statements `snoc` PHPStatementReturn ret
-
 genPHPDecl :: MonadState (DList PHPDecl) m => Definition -> m ()
 genPHPDecl (Data name _ constructors) = undefined
 genPHPDecl (Binding name value)
   = let functionDetails = toFunction value
     in case functionDetails of
-      Nothing -> do
-        let (assign,decls) = runState (genPHPAssignment name value) empty
-        modify $ flip append (fmap PHPDeclStatement $ decls `snoc` assign)
-      Just (body,args) ->
-        modify . flip snoc $ PHPDeclFunc (phpId name) (fmap phpId args) (genFunctionBody body)
+      Nothing -> modify . flip snoc . PHPDeclStatement $ runReader (genPHPAssignment name value) []
+      Just (body,args) -> do
+        let args' = fmap phpId args
+        modify . flip snoc $ PHPDeclFunc (phpId name) args' [PHPStatementReturn $ runReader (genPHPExpr body) args']
 
 genPHPLiteral :: Literal -> PHPLiteral
 genPHPLiteral (LitInt i) = PHPInt i
 genPHPLiteral (LitString s) = PHPString s
 genPHPLiteral (LitChar c) = PHPString [c]
 
-genPHPExpr :: MonadState (DList PHPStatement) m => Expr -> m PHPExpr
+genPHPExpr :: MonadReader [PHPId] m => Expr -> m PHPExpr
 genPHPExpr (Id name) = pure . PHPExprVar $ phpId name
 genPHPExpr (Lit lit) = pure . PHPExprLiteral $ genPHPLiteral lit
 genPHPExpr (Prod name args) = undefined
-genPHPExpr (App f x) = PHPExprFunctionCall <$> genPHPExpr f <*> (pure <$> genPHPExpr x)
-genPHPExpr f@(Abs name _)
-  = let (body,args) = fromJust $ toFunction f
-    in PHPExprFunction (fmap phpId args) <$> (pure . PHPStatementReturn <$> genPHPExpr body)
+genPHPExpr (App f x) = do
+  f' <- genPHPExpr f
+  x' <- genPHPExpr x
+  pure $ PHPExprFunctionCall f' [x']
+genPHPExpr (Abs name body) = do
+  scope <- ask
+  let name' = phpId name
+  body' <- local (name' :) $ genPHPExpr body
+  pure $ PHPExprFunction [name'] (Just scope) [PHPStatementReturn body']
 genPHPExpr (Let var value rest) = do
-  assign <- genPHPAssignment var value
-  modify $ flip snoc assign
-  genPHPExpr rest
-genPHPExpr (Case val branches) = undefined
-genPHPExpr (Error str) = pure $ PHPExprFunctionCall (PHPExprFunction [] [PHPStatementThrow $ PHPExprNew $ phpId "Exception"]) []
+  assignment <- genPHPAssignment var value
+  rest' <- local (phpId var :) $ genPHPExpr rest
+  scope <- ask
+  pure $ PHPExprFunctionCall (PHPExprFunction [] (Just scope) [assignment, PHPStatementReturn rest']) []
+genPHPExpr (Case val branches) = do
+  val' <- genPHPExpr val
+  branches' <- join . N.toList <$> traverse (genBranch val') branches
+  scope <- ask
+  pure $ PHPExprFunctionCall (PHPExprFunction [] (Just scope) branches') []
+  where
+    genBranch :: MonadReader [PHPId] m => PHPExpr -> (Pattern,Expr) -> m [PHPStatement]
+    genBranch val (PatId name,res) = do
+      let name' = phpId name
+      res' <- local (name' :) $ genPHPExpr res
+      pure [PHPStatementExpr $ PHPExprAssign name' val, PHPStatementReturn res']
+    genBranch val (PatCon a1 a2,res) = undefined
+    genBranch val (PatLit lit,res) = do
+      res' <- genPHPExpr res
+      pure [PHPStatementIfThenElse (PHPExprBinop Equal val (PHPExprLiteral $ genPHPLiteral lit)) [PHPStatementReturn res'] Nothing]
+genPHPExpr (Error str) = pure $ PHPExprFunctionCall (PHPExprFunction [] Nothing [PHPStatementThrow $ PHPExprNew $ phpId "Exception"]) []
 
-genPHPAssignment :: MonadState (DList PHPStatement) m => Identifier -> Expr -> m PHPStatement
+genPHPAssignment :: MonadReader [PHPId] m => Identifier -> Expr -> m PHPStatement
 genPHPAssignment name value = PHPStatementExpr . PHPExprAssign (phpId name) <$> genPHPExpr value
