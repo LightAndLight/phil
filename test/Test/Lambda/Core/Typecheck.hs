2aacc7e8799c27096c34851b74e5f17c76c5db33
Isaac Elliott <isaace71295@gmail.com>
2017-01-08 21:18:50 +1000

    Added stubs for typecheck tests

---
+++ test/Test/Lambda/Core/Typecheck.hs
@@ -0,0 +1,11 @@
+module Test.Lambda.Core.Typecheck (typecheckSpec) where
+
+import           Lambda.Core.AST
+import           Lambda.Core.Typecheck
+
+import           Test.Hspec
+
+typecheckSpec = describe "Lambda.Core.Typecheck" $
+  describe "w" $ do
+    describe "success" $ pure ()
+    describe "failure" $ pure ()
