6915f5e38ac4a129ec3419b720ad3544ff6671f6
Isaac Elliott <isaace71295@gmail.com>
2017-01-09 20:10:30 +1000

    Made lensy tests a little better

--- test/Test/Lambda/Core/Typecheck.hs
+++ test/Test/Lambda/Core/Typecheck.hs
@@ -12,10 +12,10 @@ typecheckSpec = describe "Lambda.Core.Typecheck" $
   describe "w" $ do
     describe "success" $ do
       it "\\x. x : forall a. a -> a" $
-        runW (ast (_Abs' "x" . _Id' "x")) `shouldSatisfy` has (_Right . _Forall' ["t0"] . _TyFun' (_TyVar # "t0") (_TyVar # "t0"))
+        runW (_Abs' "x" # _Id # "x") `shouldSatisfy` has (_Right . _Forall' ["t0"] . _TyFun . only (_TyVar # "t0", _TyVar # "t0"))
       it "\\x. \\y. x : forall a b. a -> b -> a" $
-        runW (ast (_Abs' "x" . _Abs' "y" . _Id' "x"))
-          `shouldSatisfy` has (_Right . _Forall' ["t0","t1"] . _TyFun' (_TyVar # "t0") (_TyFun # (_TyVar # "t1", _TyVar # "t0")))
+        runW (_Abs' "x" # _Abs' "y" # _Id # "x")
+          `shouldSatisfy` has (_Right . _Forall' ["t0","t1"] . _TyFun . only (_TyVar # "t0", _TyFun # (_TyVar # "t1", _TyVar # "t0")))
     describe "failure" $ do
       it "\\x. x x" $
         runW (_Abs' "x" # _App # (_Id # "x", _Id # "x")) `shouldSatisfy` has (_Left . _OccursError)
