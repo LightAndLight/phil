1259de617014c303215826d23a63b14034b2e030
Isaac Elliott <isaace71295@gmail.com>
2017-03-23 23:25:17 +1000

    Use nix

---
+++ hindley-milner.nix
@@ -0,0 +1,26 @@
+{ mkDerivation, alex, array, base, bifunctors, containers
+, directory, dlist, filepath, free, happy, haskeline, hspec, lens
+, mtl, optparse-applicative, QuickCheck, semigroups, stdenv
+}:
+mkDerivation {
+  pname = "hindley-milner";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    array base bifunctors containers dlist lens mtl semigroups
+  ];
+  libraryToolDepends = [ alex happy ];
+  executableHaskellDepends = [
+    base bifunctors containers directory filepath free haskeline lens
+    mtl optparse-applicative semigroups
+  ];
+  testHaskellDepends = [
+    base containers hspec lens mtl QuickCheck semigroups
+  ];
+  testToolDepends = [ alex happy ];
+  homepage = "https://github.com/githubuser/hindley-milner#readme";
+  description = "Initial project template from stack";
+  license = stdenv.lib.licenses.bsd3;
+}
