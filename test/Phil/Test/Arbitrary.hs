ebcdbec6b0099b965c5aa92c68013be04c9f32eb
Isaac Elliott <isaace71295@gmail.com>
2017-11-01 18:04:55 +1000

    Tests compile (and pass :) )

--- test/Phil/Test/Arbitrary.hs
+++ test/Phil/Test/Arbitrary.hs
@@ -1,18 +1,27 @@
 module Phil.Test.Arbitrary where
 
-import           Phil.Core.AST hiding (Identifier)
-import           Test.QuickCheck
+import Data.Text (pack)
 
-instance Arbitrary Prim where
-  arbitrary = elements [Int,String,Char,Bool]
+import           Phil.Core.AST.Identifier
+import           Phil.Core.AST.Types
+import           Test.QuickCheck
 
 instance Arbitrary Type where
   arbitrary = resize 5 $ sized sizedType
 
+instance Arbitrary Ctor where
+  arbitrary = Ctor . pack <$> arbitrary
+
+instance Arbitrary Ident where
+  arbitrary = Ident . pack <$> arbitrary
+
+instance Arbitrary TyCon where
+  arbitrary = oneof [ pure FunCon, TypeCon <$> arbitrary ]
+
 sizedType :: Int -> Gen Type
 sizedType n
   | n < 0 = error "No tree of size zero"
-  | n == 0 = TyPrim <$> arbitrary
+  | n == 0 = oneof [ TyCon <$> arbitrary, TyVar <$> arbitrary ]
   | otherwise
       = oneof
           [ TyFun <$> sizedType (n - 1) <*> sizedType (n - 1)
